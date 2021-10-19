8eb94c2e08800270525fcc2fe2b6c7b7a39a87e7
Edoardo Nannotti <edonan@gmail.com>
2021-10-19 12:09:20 +0200

    dynamic gap widths

--- components/PlayGapSelect.vue
+++ components/PlayGapSelect.vue
@@ -1,8 +1,9 @@
 <template>
   <v-select
     dense
-    style="display:inline-block"
-    :items="shuffle(token.rights.concat(token.wrongs))"
+    hide-details
+    :style="'display:inline-block; width: ' + gap_width"
+    :items="choices"
     v-model="value"
     :success="success === true"
     :error="success === false"
@@ -17,6 +18,46 @@ export default {
     value: undefined
   }),
   computed: {
+    choices: function() {
+      /**
+       * Return the array of the possible answers to choose from,
+       * in random order.
+       * @returns {str[]}
+       */
+      return this.shuffle(
+        this.token.rights.concat(
+          this.token.wrongs
+        )
+      )
+    },
+    longer_choice_length: function() {
+      /**
+       * Return the length in character of the longest
+       * answer that one can choice (to calculate gap width)
+       * @returns {int}
+       */
+      return Math.max(
+        ...this.choices.map(
+          s => s.length
+        )
+      )
+    },
+    gap_width: function() {
+      /**
+       * Returns the css value used to set the gap width
+       * @returns {str}
+       */
+      return this.longer_choice_length + 7 + 'ch';
+      // FIXME
+      // heuristic calculation: to display the string "0"
+      // into the v-select, a minimum width of 8ch was
+      // found to be required. Hence, assuming that any
+      // character has roughly the same width of "0", we
+      // do the math on the longest answer's length.
+      // Note that this assumption is by no mean accurate
+      // and changing the styling or the icon slots etc.
+      // of the v-select will have an inpact on it.
+    },
     success: function() {
       if (this.token.rights.includes(this.value)) {
         return true
