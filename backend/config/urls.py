3ad70ceb8eb2eae5197f665c533f7f52683ae9d4
Baptiste Darthenay <baptiste@darthenay.fr>
2022-07-27 17:27:33 +0200

    feat(api): Add v1 apps URLs

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,11 +1,10 @@
+from apps.exercises.views import SheetViewSet
 from dj_rest_auth.registration.views import VerifyEmailView
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import (TokenObtainPairView,
                                             TokenRefreshView, TokenVerifyView)
-
-from apps.exercises.views import SheetViewSet
 
 router = DefaultRouter()
 router.register("sheets", SheetViewSet)
@@ -14,6 +13,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/auth/", include("dj_rest_auth.urls")),
     path("api/auth/register/", include("dj_rest_auth.registration.urls")),
+    path("api/v1/", include(router.urls)),
     path(
         "dj-rest-auth/account-confirm-email/",
         VerifyEmailView.as_view(),
