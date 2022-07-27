3403dd8a1c832dbc4a0bf2f632ca95fbd191f0e9
Edoardo Nannotti <edonan@gmail.com>
2022-07-27 16:56:40 +0200

    python 3.9, +SessionAuthentication

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -110,6 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.SessionAuthentication",
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ],
     "DEFAULT_PERMISSION_CLASSES": [
