daff0729d8ea748251f54772c93c6033caa66145
Edoardo Nannotti <edonan@gmail.com>
2022-07-28 17:22:01 +0200

    login w/ redirect

--- client/pages/login.vue
+++ client/pages/login.vue
@@ -1,20 +1,33 @@
 <template>
   <v-row>
     <v-col class="text-center">
-      <v-form>
+      <v-form
+        ref="login_form"
+        v-model="valid"
+        lazy-validation
+      >
         <v-text-field
           v-model="email"
           label="E-mail"
+          :rules="[ v => /.+@.+\..+/.test(v) || 'E-mail must be valid' ]"
           required
         ></v-text-field>
         <v-text-field
           v-model="password"
           label="Password"
+          :rules="[ v => v !== '' || 'Required field' ]"
           required
         ></v-text-field>
         <v-btn
           @click="login"
         >Login</v-btn>
+        <v-alert
+          class="mt-2"
+          type="error"
+          v-show="!!error_message"
+        >
+          {{ error_message }}
+        </v-alert>
       </v-form>
     </v-col>
   </v-row>
@@ -23,23 +36,40 @@
   export default {
     data: () => ({
       email: '',
-      password: ''
+      password: '',
+      valid: undefined,
+      error_message: '',
     }),
     methods: {
+      validate() {
+        return this.$refs.login_form.validate()
+      },
+      login_error_hdl(e) {
+        this.error_message = e.join('\n');
+      },
       login: async function() {
+        if (!this.validate()) return;
         try {
           const response = await this.$auth.login({
             email: this.email,
             password: this.password
           });
-          console.log("neniu eraro");
-          console.log(response);
-          //redirect(
-          //  this.$store.state.authenticator.redirectUrl
-          //);
-        } catch {
-          console.log("error handling");
-          console.log(response);
+
+          // this "if" is needed because even on failure vue-auth-jwt returns
+          // an array of errors instead of throwing an exception.
+          // Open issue: https://github.com/johnckealy/vue-auth-jwt/issues/2
+
+          if (response.status && response.status == 200) {
+            this.$router.push({
+              path: this.$store.state.authenticator.redirectUrl
+            });
+            return;
+          }
+          this.login_error_hdl(response);
+
+        } catch(e) {
+          // to be future proof when vue-auth-jwt will be fixed
+          this.login_error_hdl(e.errorMessages);
         }
       }
     }
