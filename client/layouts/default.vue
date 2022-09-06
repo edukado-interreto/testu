8e03860bf1ce416cf90dc0bf1f0fe48da85e098b
Edoardo Nannotti <edonan@gmail.com>
2022-09-06 15:58:09 +0200

    main layout

--- client/layouts/default.vue
+++ client/layouts/default.vue
@@ -3,39 +3,197 @@
     <v-app-bar
       app
       clipped-right
+      clipped-left
       height="72"
       flat
     >
-      <v-app-bar-nav-icon @click.stop="nav_drawer.show = !nav_drawer.show" disabled />
+      <v-app-bar-nav-icon @click.stop="nav_drawer.show = !nav_drawer.show"/>
       <v-toolbar-title v-text="title" />
       <v-spacer />
-      <v-btn
-        icon
-        disabled
+      <v-menu
+        offset-y
       >
-        <v-icon>mdi-account</v-icon>
-      </v-btn>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn
+            text
+            v-bind="attrs"
+            v-on="on"
+            class="text-none px-1 mr-1"
+          >
+            <v-icon class="mr-1">mdi-translate</v-icon>
+            en
+            <v-icon class="ml-1">mdi-chevron-down</v-icon>
+          </v-btn>
+        </template>
+
+        <v-list>
+          <v-list-item>
+            <v-list-item-content>
+              <v-list-item-title>English</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-content>
+              <v-list-item-title>Esperanto</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-content>
+              <v-list-item-title>French</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-content>
+              <v-list-item-title>Italian</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+
+      </v-menu>
+
+      <template
+        v-if="!DEBUG_logged_in"
+      >
+        <v-btn
+          @click="DEBUG_logged_in = true"
+          class="mr-1 text-none"
+        >Log in</v-btn>
+        <v-btn
+          @click="DEBUG_logged_in = true"
+          color="primary"
+          class="text-none"
+        >Sign up</v-btn>
+      </template>
+
+      <v-menu
+        v-if="DEBUG_logged_in"
+        offset-y
+      >
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn
+            icon
+            v-bind="attrs"
+            v-on="on"
+          >
+            <v-icon>mdi-account</v-icon>
+          </v-btn>
+        </template>
+
+        <v-card>
+          <v-list>
+            <v-list-item>
+              <v-list-item-avatar>
+                <v-icon>mdi-account</v-icon>
+                <!--img
+                  src="https://cdn.vuetifyjs.com/images/john.jpg"
+                  alt="John"
+                -->
+              </v-list-item-avatar>
+
+              <v-list-item-content>
+                <v-list-item-title>John Smith</v-list-item-title>
+                <v-list-item-subtitle>john@example.org</v-list-item-subtitle>
+              </v-list-item-content>
+
+            </v-list-item>
+          </v-list>
+
+          <v-divider></v-divider>
+
+          <v-list>
+
+            <v-list-item
+              to="/profile"
+              router
+              exact
+            >
+              <v-list-item-action>
+                <v-icon>mdi-cog</v-icon>
+              </v-list-item-action>
+              <v-list-item-content>
+                <v-list-item-title>Account settings</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+
+            <v-list-item
+              @click="DEBUG_logged_in = false"
+            >
+              <v-list-item-action>
+                <v-icon>mdi-logout</v-icon>
+              </v-list-item-action>
+              <v-list-item-content>
+                <v-list-item-title>Logout</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+
+          </v-list>
+
+        </v-card>
+      </v-menu>
+
+
+
     </v-app-bar>
     <v-navigation-drawer
       v-model="nav_drawer.show"
-      mini-variant
+      disable-resize-watcher
       app
+      clipped
     >
       <v-list>
+
         <v-list-item
-          v-for="(item, i) in nav_drawer.items"
-          :key="i"
-          :to="item.to"
+          to="/"
           router
           exact
         >
           <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
+            <v-icon>mdi-home</v-icon>
           </v-list-item-action>
           <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
+            <v-list-item-title>Home</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
+
+        <v-list-item
+          to="/learn"
+          router
+          exact
+        >
+          <v-list-item-action>
+            <v-icon>mdi-chart-bar</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>My learning</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-list-item
+          to="/teach"
+          router
+          exact
+        >
+          <v-list-item-action>
+            <v-icon>mdi-briefcase</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>My teaching</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-list-item
+          to="/about"
+          router
+          exact
+        >
+          <v-list-item-action>
+            <v-icon>mdi-information</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>About</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+
       </v-list>
     </v-navigation-drawer>
     <v-main>
@@ -56,20 +214,9 @@ export default {
   name: 'default',
   data () {
     return {
+      DEBUG_logged_in: true,
       nav_drawer: {
-        show: false,
-        items: [
-          {
-            icon: 'mdi-apps',
-            title: 'Home page',
-            to: '/'
-          },
-          {
-            icon: 'mdi-chart-bubble',
-            title: 'About',
-            to: '/inspire'
-          }
-        ],
+        show: true
       },
       title: 'testu.eu'
     }
