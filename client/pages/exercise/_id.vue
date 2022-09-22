fab0051d36e40fb8536b48e1a869259ede3caeaf
Edoardo Nannotti <edonan@gmail.com>
2022-09-22 14:44:57 +0200

    retrieve exercise

---
+++ client/pages/exercise/_id.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    {{ id }}
+    {{ exercise }}
+  </div>
+</template>
+<script>
+  export default {
+    name: 'Exercise',
+    data: () => ({
+    }),
+    async asyncData({ route, params }) {
+      const id = (process.server) ? route.params.id : params.id;
+      let exercise;
+
+      if (id) {
+        exercise = await (
+          await fetch(`/api/v1/sheets/${id}/`)
+        ).json()
+      }
+
+      return {
+        id,
+        exercise
+      }
+    }
+  }
+</script>
