c7ee6b8349237ce59d0b4940e87344c862050036
Edoardo Nannotti <edonan@gmail.com>
2022-09-23 17:26:08 +0200

    disable ssr to fix login issues

--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -2,7 +2,7 @@ import colors from 'vuetify/es5/util/colors'
 
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
-  ssr: true,
+  ssr: false,
 
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
